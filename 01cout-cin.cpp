5d61d38209d5a50f4e5edc40be14ba6d374cfe6d
KimYeongJIn <125858342+MDandersonm@users.noreply.github.com>
2024-09-30 15:54:54 +0900

    1강 입출력, namespace

---
+++ 01cout-cin.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main01() {
+	/*주석방법1*/
+	//주석방법2
+
+
+	std::cout << "헬로" <<"a" 
+		<<"입니다"
+		<< std::endl;
+
+	//표준 입력스트림
+	int a;
+	char str[100];
+	std::cin >> a >> str; //연속해서 입력하기
+
+	std::cout << a <<  str << std::endl;
+
+	return 0;
+}
